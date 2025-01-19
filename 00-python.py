bfa202e61618b8aaecdb8e6e4cfdc7c1a3f4f4cb
citlalli-rvr <crivera14@ucol.mx>
2025-01-19 17:28:20 -0600

    Update and rename 00-python to 00-python.py

---
+++ 00-python.py
@@ -0,0 +1,49 @@
+# https://www.python.org/
+#https://medium.com/@diego.coder/variables-y-tipos-de-datos-en-python-str-int-bool-etc-45ac17e6acc7
+#esto es un comentario simple en python
+"""
+esto es un 
+comentario
+en bloque 
+"""
+
+mi_variable= "Variable"
+
+MI_CONSTANTE= "mi constante" #no existen constantes, pero se indican con mayusculas 
+"""
+Los datos prmitivos en python son 
+Strings--> representa cadena de texto, inmutables
+   --> métodos y propiedades para trabajarlas:
+         °len()- longitud de la cadena
+         °upper() lower()- conversion mayusculas y minusculas 
+         °capitalize()- primer carácter a mayúsculas
+         °title()- primer caracter de cada palabra a mayúsculas
+
+
+Integers-->  representa número enteros, positivos, negativos o ceros
+     --> métodos y propiedades para trabajarlos:
+         °bit_length()- devuelve el npumero de bits necesarios para representar en binario
+         °abs()- Devuelve el valor absoluto del entero 
+         °round()- redondea el entero a un número 
+         °int(dato)- convierte otro tipo de dato a entero
+         °bin(),oct(),hex()- de entero a binaria, octal o hexadecimal
+Floats--> números con parte fraccionaria
+      --> métodos y propiedades para trabajarlos:
+          °is_integer()- comprueba si la parte decimal es igual a 0
+          °max(), min()- valor máximo o minimo entre barios números
+          °math.sqrt()- calcula raíz cuadrada
+booleans--> representa valores de verdad True o False
+** utilizados en expresiones lógicas, toma de decisiones en estructuras de control de flujo
+
+
+"""
+my_int= 1
+my_floats= 3.14
+my_strings= "cadena de texto"
+my_bool= True
+my_bool= False
+
+print ("!Hola, [pyhton]!")
+
+       
+
