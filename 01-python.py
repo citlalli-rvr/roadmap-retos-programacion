5284d01f4bf93f9e549c402130839232d4e3e05b
citlalli-rvr <crivera14@ucol.mx>
2025-01-19 18:06:54 -0600

    Create 01-python.py

---
+++ 01-python.py
@@ -0,0 +1,72 @@
+#operadores aritméticos
+#operaciones matematicas en variables y valores
+
+print(f"suma { 2 + 5}")
+print(f"resta {5 - 2}")
+print(f"multiplicacion {3 * 9 }")
+print(f"division {9/3}")
+#modulo- devulve el residuo de la division
+print(f"modulo {4 % 4}")
+print(f"potencia {3 ** 2}")
+
+#operadores de asignación
+#asignar valores a variables
+simple= "asignación simple con ="
+
+#combinacion de asignacion y operación (compuesta)
+num1=5
+print(num1)
+num1 +=3 #suma al valor
+print(num1)
+num2=10
+num2 -=5 #resta al valor
+print(num2)
+num3=5
+num3 *=2 #multiplicación al valor
+print(num3)
+num4=7
+num4 /=2 #división al valor
+print(num4)
+num5= 15
+num5 %=4 #modulo al valor
+print(num5)
+num6=7
+num6 //=2 #división entera
+print(num6)
+
+#operadores de comparación
+print( num1 == num2) #compara si son valores iguales (arroja True o False)
+print(num3 != num4) #compara si sin valores desiguales 
+print(num1 < num2) #menor que
+print(num3 > num4) #mayor que
+print(num5 >= num6) #mayor o igual que
+print(num1 <= num2) #menor o igual que
+
+#operadores lógicos
+print(num1> 0 and num2< 20) #True si ambas son verdaderas de lo contrario False
+print(num3> 10 or num4<20) #True si al menos una condición es verdadera
+condicion= False
+print(not condicion) #invierte el valor
+
+#operadores a nivel de bits
+#el operador es aplicado bit x bit (considerandose en binario)
+print(num1|num2) #or bit a bit
+print(num3^num4) #or exclusivo bit a bit
+print(num5 & num6) #and bit a bit
+print(num2 << 3) #desplaza num2 a 3 bits a la izquierda
+print(num2 >> 3) #Desplaza num3 a 3 bits a la derecha
+print(~num2) #obtienes los bits de num2 invertidos
+
+
+#operadores de pertenencia
+#comprobar si un valor o variable se encuentra en una secuencia
+
+#operadores de identidad
+
+#Estructuras de control
+
+
+
+
+
+
