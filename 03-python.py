b093c8f9400ee8a8a33bbee0e66b34cbc705c217
citlalli-rvr <crivera14@ucol.mx>
2025-04-29 17:22:32 -0600

    Create 03-python.py

---
+++ 03-python.py
@@ -0,0 +1,10 @@
+#ESTRUCTURAS DE DATOS
+#estructuras soportadas por defecto 
+#operaciones de inserción, borrado, actualización y ordenación
+
+#consultar: https://docs.python.org/es/3/tutorial/datastructures.html
+
+#Listas
+#Tuplas
+#Set
+#Diccionarios
